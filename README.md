19dc3f0bd8dcd5cd837dae17c3cd868a0472e36c
Rares Croicia <rarescroicia@gmail.com>
2023-10-25 15:29:13 +0300

    Add exercises and READMEs for every part
    Signed-off-by: Rares Croicia <rarescroicia@gmail.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+# Workshop IT Department 2023
+
+## Requirements
+
+In order to solve the following exercises, you need to install [node.js](https://nodejs.org/en/download) and [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+
+## Exercises
+We have prepared for you some [backend](backend/README.md) and [frontend](frontend/README.md) exercises so you can practice for the technical part of the recruiting process.\
+You will have to implement a **TODO** application, where you can see, add, delete and update your todos.\
+You can also find the [solutions](https://github.com/LSAC-ONG/workshop2023/tree/solutie) for the exercises, but we recommend that you give it a try before looking at them.
+
+## How to run
+
+- In order to run the frontend, you have to run from the frontend directory the following command:
+    - `npm run dev`
+- In order to run the backend, you have to run from the backend directory the following command:
+    - `npm start`
--- backend/index.js
+++ backend/index.js
@@ -7,54 +7,58 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// In real life, this would be stored in a database.
+/*
+  In real world applications, we would use a database to store the todos.
+  For this exercise, we will use the array "todosArray" to store them.
+*/
 var todosArray = [];
 
+
+/*
+  TODO 1: Implement the endpoint `GET /todos` to send all todos to the client.
+*/
 app.get("/todos", (req, res) => {
-  // TODO 1: Send all todos to frontend.
-
-  res.status(200).json(todosArray);
+  res.status(500).send({message: "Not implemented"});
 });
 
+/*
+  TODO 2: Implement the endpoint `POST /todos` to add new todo to todosArray.
+  The request body will contain the new todo text.
+  Example request body:
+  
+  {
+    todo: "Buy milk"
+  }
+
+*/
 app.post("/todos", (req, res) => {
-  // TODO 2: Add new todo to todosArray.
-
-  const todo = req.body;
-
-  todosArray.push(todo);
-
-  res.status(201).json(todo);
+  res.status(500).send({message: "Not implemented"});
 });
 
+/*
+  TODO 3: Implement the endpoint `DELETE /todos/:text` to delete todo from todosArray.
+*/
 app.delete("/todos/:text", (req, res) => {
-  // TODO 3: Delete todo from todosArray.
-
-  const text = req.params.text;
-
-  todosArray = todosArray.filter((item) => item.todo !== text);
-
-  res.status(204).send();
+  res.status(500).send({message: "Not implemented"});
 });
 
+/*
+  TODO 4: Implement the endpoint `PATCH /todos/:text` to update todo in todosArray.
+  The request body will contain the new todo text.
+  Example request body:
+  
+  {
+    todo: "Don't buy milk"
+  }
+
+*/
 app.patch("/todos/:text", (req, res) => {
-  // TODO 4: Update todo in todosArray.
-
-  const text = req.params.text;
-  const todo = req.body;
-
-  todosArray = todosArray.map((item) => {
-    if (item.todo === text) {
-      return todo;
-    } else {
-      return item;
-    }
-  });
-
-  res.status(200).json(todo);
+  res.status(500).send({message: "Not implemented"});
 });
 
 // Start server
 const port = 3000;
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
